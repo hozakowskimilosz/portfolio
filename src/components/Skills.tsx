6719530dbfb6445b8221df6444e6a5e5c55e87df
milosz.hozakowski <mhozakowski@gmail.com>
2024-03-11 15:06:48 +0100

    added skills component

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,44 @@
+import { ReactNode } from "react";
+
 function Skills() {
+  const skills = [
+    "React",
+    "TypeScript",
+    "Redux",
+    "AWS",
+    "Git",
+    "Python",
+    "TailwindCSS",
+    "ChakraUI",
+    "FramerMotion",
+  ];
+
   return (
-    <div id="skills" className="h-screen flex items-center justify-center">
-      Skills
+    <div
+      id="skills"
+      className="flex h-screen flex-col items-center justify-center"
+    >
+      <div className="flex flex-col gap-4">
+        <h2 className="text-6xl font-bold">Skills</h2>
+
+        <div className="flex flex-wrap gap-4">
+          {skills.map((skill) => (
+            <Skill>{skill}</Skill>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+interface SkillProps {
+  children: ReactNode;
+}
+
+function Skill({ children }: SkillProps) {
+  return (
+    <div className="rounded-full bg-gray-100 px-5 py-3 text-2xl">
+      {children}
     </div>
   );
 }

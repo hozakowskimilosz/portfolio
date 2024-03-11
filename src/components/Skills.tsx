8e368f12f9389f9d29dc22c65e19380421a6864c
milosz.hozakowski <mhozakowski@gmail.com>
2024-03-11 15:41:32 +0100

    added nice while in view animation

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
+import { ReactNode, useEffect, useRef } from "react";
 
 function Skills() {
   const skills = [
@@ -13,17 +14,42 @@ function Skills() {
     "FramerMotion",
   ];
 
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    } else {
+      mainControls.start("hidden");
+    }
+  }, [isInView, mainControls]);
+
   return (
     <div
       id="skills"
-      className="flex h-screen flex-col items-center justify-center"
+      className="flex h-screen flex-col items-center justify-center px-5"
     >
-      <div className="flex flex-col gap-4">
-        <h2 className="text-6xl font-bold">Skills</h2>
+      <div className="flex flex-col gap-4 md:w-[40rem]">
+        <h2 className="text-6xl font-bold transition-all hover:text-7xl hover:text-primary-light">
+          Skills
+        </h2>
 
         <div className="flex flex-wrap gap-4">
-          {skills.map((skill) => (
-            <Skill>{skill}</Skill>
+          {skills.map((skill, idx) => (
+            <motion.div
+              ref={ref}
+              variants={{
+                hidden: { opacity: 0, x: -300 },
+                visible: { opacity: 1, x: 0 },
+              }}
+              initial="hidden"
+              animate={mainControls}
+              transition={{ duration: 0.5, delay: 0.1 * idx }}
+            >
+              <Skill>{skill}</Skill>
+            </motion.div>
           ))}
         </div>
       </div>
@@ -37,7 +63,7 @@ interface SkillProps {
 
 function Skill({ children }: SkillProps) {
   return (
-    <div className="rounded-full bg-gray-100 px-5 py-3 text-2xl">
+    <div className="rounded-full border border-transparent bg-gray-100 px-8 py-3 text-2xl transition-all hover:scale-105 hover:border-primary">
       {children}
     </div>
   );

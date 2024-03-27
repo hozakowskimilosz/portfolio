77ec51a2902fcecc54709945041f586ba302c257
milosz.hozakowski <mhozakowski@gmail.com>
2024-03-27 16:54:55 +0100

    added icons to skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,15 +3,114 @@ import Reveal from "./ui/Reveal";
 
 function Skills() {
   const skills = [
-    "React",
-    "TypeScript",
-    "Redux",
-    "AWS",
-    "Git",
-    "Python",
-    "TailwindCSS",
-    "ChakraUI",
-    "FramerMotion",
+    {
+      name: "React",
+      icon: (
+        <img
+          className="w-8"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+        />
+      ),
+    },
+    {
+      name: "TypeScript",
+      icon: (
+        <img
+          className="w-8"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Typescript.svg/96px-Typescript.svg.png"
+        />
+      ),
+    },
+    {
+      name: "Redux",
+      icon: (
+        <img
+          className="w-8"
+          src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/redux-icon.png"
+        />
+      ),
+    },
+    {
+      name: "NodeJS",
+      icon: (
+        <img
+          className="w-8"
+          src="https://static-00.iconduck.com/assets.00/nodejs-icon-2048x2048-rueyo8fw.png"
+        />
+      ),
+    },
+    {
+      name: "Python",
+      icon: (
+        <img
+          className="w-8"
+          src="https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png"
+        />
+      ),
+    },
+    {
+      name: "Git",
+      icon: (
+        <img
+          className="w-8"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2048px-Git_icon.svg.png"
+        />
+      ),
+    },
+    {
+      name: "TailwindCSS",
+      icon: (
+        <img
+          className="w-8"
+          src="https://w7.pngwing.com/pngs/293/485/png-transparent-tailwind-css-hd-logo.png"
+        />
+      ),
+    },
+    {
+      name: "ChakraUI",
+      icon: (
+        <img
+          className="w-8"
+          src="https://avatars.githubusercontent.com/u/54212428?s=280&v=4"
+        />
+      ),
+    },
+    {
+      name: "FramerMotion",
+      icon: (
+        <img
+          className="w-8"
+          src="https://cdn.worldvectorlogo.com/logos/framer-motion.svg"
+        />
+      ),
+    },
+    {
+      name: "SQL",
+      icon: (
+        <img
+          className="w-8"
+          src="https://static-00.iconduck.com/assets.00/sql-database-sql-azure-icon-1955x2048-4pmty46t.png"
+        />
+      ),
+    },
+    {
+      name: "AWS",
+      icon: (
+        <img
+          className="w-8"
+          src="https://static-00.iconduck.com/assets.00/general-awscloud-icon-2048x2048-3trjae80.png"
+        />
+      ),
+    },
+    {
+      name: "Netlify",
+      icon: (
+        <img
+          className="w-8"
+          src="https://cdn.iconscout.com/icon/free/png-256/free-netlify-3628945-3030170.png"
+        />
+      ),
+    },
   ];
 
   return (
@@ -27,7 +126,10 @@ function Skills() {
         <div className="flex flex-wrap gap-4">
           {skills.map((skill, idx) => (
             <Reveal idx={idx}>
-              <Skill>{skill}</Skill>
+              <Skill>
+                <p>{skill.name}</p>
+                <div>{skill.icon}</div>
+              </Skill>
             </Reveal>
           ))}
         </div>
@@ -42,7 +144,7 @@ interface SkillProps {
 
 function Skill({ children }: SkillProps) {
   return (
-    <div className="rounded-md border-2 border-transparent bg-gray-100 px-8 py-3 text-2xl transition-all hover:scale-105 hover:border-primary">
+    <div className="flex items-center gap-4 rounded-md border-2 border-transparent bg-gray-100 px-8 py-3 text-2xl transition-all hover:scale-105 hover:border-primary">
       {children}
     </div>
   );

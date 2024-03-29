5865a88a9fddc95bc1479172e6c85232bc6482d2
milosz.hozakowski <mhozakowski@gmail.com>
2024-03-29 17:38:04 +0100

    started work on projects section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,11 @@
 function Projects() {
   return (
     <div id="projects" className="flex h-screen items-center justify-center">
-      Projects
+      <div className="flex flex-col gap-4 md:w-[40rem]">
+        <h2 className="text-6xl font-bold transition-all hover:tracking-wider hover:text-primary-light">
+          My Projects
+        </h2>
+      </div>
     </div>
   );
 }
